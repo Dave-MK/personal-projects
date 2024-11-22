b1893f470e1383729fa9241ead81652d6d8fa9ba
Dave-MK <davidmkilgallon@gmail.com>
2024-11-22 02:22:28 +0000

    Add quesions and basic functionality

--- Python/quiz_game.py
+++ Python/quiz_game.py
@@ -6,3 +6,35 @@ if playing != "y":
 	quit()
 
 print("Okay! Let's Play: ")
+
+# Question 1
+answer = input("Who is the Allfather? ").lower()
+
+if answer == "odin":
+	print("Correct!")
+else:
+    print("Incorrect!")
+
+# Question 2
+answer = input("Who was the god of thunder? ").lower()
+
+if answer == "thor":
+	print("Correct!")
+else:
+    print("Incorrect!")
+    
+# Question 3
+answer = input("What is the name of Thors hammer? ").lower()
+
+if answer == "mjolnir":
+	print("Correct!")
+else:
+    print("Incorrect!")
+    
+# Question 4
+answer = input("Who is the trickster? ").lower()
+
+if answer == "loki":
+	print("Correct!")
+else:
+    print("Incorrect!")
