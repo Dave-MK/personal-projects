941813edb3616020122e8f707ec5ee6cf902125a
Dave-MK <davidmkilgallon@gmail.com>
2024-11-22 02:43:42 +0000

    Complete quiz game and create new file for number guessing game

--- Python/quiz_game.py
+++ Python/quiz_game.py
@@ -7,34 +7,45 @@ if playing != "y":
 
 print("Okay! Let's Play: ")
 
-# Question 1
-answer = input("Who is the Allfather? ").lower()
+score = 0
 
-if answer == "odin":
+# Question 1
+answer = input("Who is the Allfather? ")
+
+if answer.lower() == "odin":
 	print("Correct!")
+	score += 1
 else:
     print("Incorrect!")
 
 # Question 2
-answer = input("Who was the god of thunder? ").lower()
+answer = input("Who was the god of thunder? ")
 
-if answer == "thor":
+if answer.lower() == "thor":
 	print("Correct!")
+	score += 1
 else:
     print("Incorrect!")
     
 # Question 3
-answer = input("What is the name of Thors hammer? ").lower()
+answer = input("What is the name of Thors hammer? ")
 
-if answer == "mjolnir":
-	print("Correct!")
+if answer.lower() == "mjolnir":
+		print("Correct!")
+		score += 1
 else:
     print("Incorrect!")
     
 # Question 4
-answer = input("Who is the trickster? ").lower()
+answer = input("Who is the trickster? ")
 
-if answer == "loki":
+if answer.lower() == "loki":
 	print("Correct!")
+	score += 1
 else:
     print("Incorrect!")
+
+cent = (score / 4) * 100
+
+print(f"You got {str(score)} questions correct!")
+print(f"Meaning you got {str(cent)}% of the questions correct!")
